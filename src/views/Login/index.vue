b89be06759f636d5fd3796ccb70310184fef761f
gentlehao <gentlehao@outlook.com>
2019-12-23 09:52:09 +0800

    放弃缓存(ˉ▽ˉ；)...

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="Login">
-    <keep-alive>
-      <router-view v-if="$route.meta.keepAlive" />
-    </keep-alive>
-    <router-view v-if="!$route.meta.keepAlive" />
+    <router-view />
   </div>
 </template>
 <script>
