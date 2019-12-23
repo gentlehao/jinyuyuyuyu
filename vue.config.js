f699aaf29e361d3f53b05704fda8e74db169c3ae
gentlehao <gentlehao@outlook.com>
2019-12-23 13:44:22 +0800

    配置代理

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,26 @@
+'use strict'
+
 module.exports = {
-    publicPath: '/',
-    outputDir: 'dist', // 打包的目录
-    lintOnSave: true, // 在保存时校验格式
-    productionSourceMap: false, // 生产环境是否生成 SourceMap
-    devServer: {
-      open: true, // 启动服务后是否打开浏览器
-      host: '0.0.0.0',
-      port: 8080, // 服务端口
-      https: false,
-      hotOnly: false,
-      proxy: null, // 设置代理
-      before: app => {}
+  publicPath: '/',
+  outputDir: 'dist', // 打包的目录
+  lintOnSave: true, // 在保存时校验格式
+  productionSourceMap: false, // 生产环境是否生成 SourceMap
+  devServer: {
+    open: false, // 启动服务后是否打开浏览器
+    host: '0.0.0.0',
+    port: 8080, // 服务端口
+    https: false,
+    hotOnly: false,
+    proxy: {
+      "/api": {
+        target: "https://39.105.164.120:3306/", // 代理路径
+        changeOrigin: true, // 将主机标头的原点更改为目标URL
+        ws: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
     },
-  }
+    before: app => { }
+  },
+}
