7ce7bfa90b7a97117cbcc5fd18e35f10c3dee3d0
unknown <gentlehao@outlook.com>
2020-01-08 22:12:22 +0800

    没写啥(～﹃～)~zZ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,22 @@
     <back-to-top></back-to-top>
     <Top></Top>
     <Header></Header>
-    <NavMenu activeIndex='1'></NavMenu>
+    <div class="nav">
+      <NavMenu activeIndex='1'></NavMenu>
+    </div>
+    <div class="content" v-loading="true" element-loading-background="rgba(255, 255, 255, 0.6)">
+      <el-carousel trigger="click" height="320px">
+        <el-carousel-item v-for="banner in 5" :key="banner" :interval="5000">
+          <router-link :to="{name:'',query:''}"><el-image class="banner"></el-image></router-link>
+        </el-carousel-item>
+      </el-carousel>
+      <div class="someNum fz-16">
+        <i class="el-icon-info"></i>
+      </div>
+      <div class="recommend">
+        
+      </div>
+    </div>
     <Footer></Footer>
   </div>
 </template>
@@ -17,6 +32,11 @@ import Footer from '@/components/Footer'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      banners: [], //轮播图
+    }
+  },
   components: {
     backToTop,
     Top,
@@ -27,3 +47,18 @@ export default {
   methods: {}
 }
 </script>
+
+<style lang="scss" scoped>
+.nav, .someNum{
+  width: 1024px;
+  margin: auto;
+}
+.banner {
+  height: 100%;
+  width: 100%;
+  background: skyblue;
+}
+.banner:hover {
+  cursor: pointer;
+}
+</style>
